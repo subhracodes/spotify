2e8c675cb0b2e3af4b0efe02c50c5d1ad9eb20c2
Subhrajeet <subhrajeetde5@gmail.com>
2025-04-27 14:46:43 +0530

    adjusted responsiveness of some ui elements

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log("let's write js");
+
+// async function main(){
+//     let a=await fetch("http://127.0.0.1:5500/songs/")
+//     let response=await a.text()
+//     console.log(response);
+    
+// }
+// main()
