7252107240bf75a9b48d7d34be4d55e92fa7cfcc
Subhrajeet <subhrajeetde5@gmail.com>
2025-05-27 14:09:51 +0530

    adding a side menu that slides left , for devices below 840px

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-// console.log("let's write js");
-
-// async function main(){
-//     let a=await fetch("http://127.0.0.1:5500/songs/")
-//     let response=await a.text()
-//     console.log(response);
-    
-// }
-// main()
+document.querySelector(".lines").addEventListener("click",()=>{
+    document.querySelector(".lines").style.visibility="hidden"
+    // document.querySelector(".blurblack").classList.toggle("openmenu")
+    document.querySelector(".blurblack").style.width="40vw";
+    document.querySelector(".menuline").style.width="98%";
+    document.querySelector(".backarrow").style.display="inline-block"
+    document.querySelector(".blurblack").style.animation="openmenu2 0.3s"
+})
+window.addEventListener("resize", () => {
+  if(window.innerWidth>841){
+    document.querySelector(".blurblack").style.width="0px";
+    document.querySelector(".blurblack").style.animation="closemenu 0.3s"
+  }
+});
+document.querySelector(".backarrow").addEventListener("click",()=>{
+    document.querySelector(".lines").style.visibility="visible"
+    document.querySelector(".blurblack").style.width="0px";
+    document.querySelector(".blurblack").style.animation="closemenu 0.3s"
+    document.querySelector(".backarrow").style.display="none";
+    // document.querySelector(".backarrow").classList.toggle("closemenu")
+})
