7763c4f2e31313d67652e8e38520b697e8f8c08c
Subhrajeet <subhrajeetde5@gmail.com>
2025-05-30 10:10:11 +0530

    creating a seekbar and adding the posters of few songs

--- script.js
+++ script.js
@@ -17,5 +17,58 @@ document.querySelector(".backarrow").addEventListener("click",()=>{
     document.querySelector(".blurblack").style.width="0px";
     document.querySelector(".blurblack").style.animation="closemenu 0.3s"
     document.querySelector(".backarrow").style.display="none";
+    document.querySelector(".menuline").style.width="0px";
     // document.querySelector(".backarrow").classList.toggle("closemenu")
 })
+
+
+//fetching the songs folder
+async function songArray(){
+  const response = await fetch('http://127.0.0.1:5500/songs/');
+  const data = await response.text();
+  // console.log(data);
+  let div=document.createElement("div");
+  div.innerHTML=data;
+  let lis=div.getElementsByTagName("li")
+  let songslist=Array.from(lis)
+  songslist.shift()
+  songslist.forEach(e=>{
+    console.log(e.getElementsByTagName("span")[0]);
+    
+  })
+  // console.log(songs);
+  let posters=[];
+  let songs=[];
+  let count=1;
+  songslist.forEach(e=>{
+    if(count%2==0){
+      posters.push(e.getElementsByTagName("span")[0].innerHTML)
+      // count++;
+    }
+    else{
+      songs.push(e.getElementsByTagName("span")[0].innerHTML)
+    }
+    count++;
+  })
+  console.log(posters);
+  console.log(songs);
+  posters.forEach(e => {
+  let post = document.createElement("div")
+  post.classList.add("card");                
+  post.innerHTML = "<img src='songs/"+e+"' class='cards'>";  
+  // console.log(`${e}`);
+                    
+  document.querySelector(".cardcont").append(post); 
+});
+
+  
+}
+songArray()
+function pause(){
+  document.querySelector(".pause").style.display="none";
+  document.querySelector(".play").style.display="inline-block";
+}
+function play(){
+  document.querySelector(".play").style.display="none";
+  document.querySelector(".pause").style.display="inline-block";
+}
